2fa83ee32cdddb4093a3ae3699b7d6f686b2e964
Tristan Dobos <tristandobos37@gmail.com>
2025-12-28 21:57:39 +0100

    added metric

--- examples/tts/audio_codec.py
+++ examples/tts/audio_codec.py
@@ -20,6 +20,7 @@ from nemo.core.config import hydra_runner
 from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
 
+adsfafa
 
 @hydra_runner(config_path="conf/audio_codec", config_name="audio_codec")
 def main(cfg):
