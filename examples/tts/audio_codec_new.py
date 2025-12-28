6a70cf2b8b7455c00afdf957e4b99a53e33c1175
Tristan Dobos <tristandobos37@gmail.com>
2025-12-28 20:54:11 +0100

    added metric

--- examples/tts/audio_codec_new.py
+++ examples/tts/audio_codec_new.py
@@ -15,7 +15,7 @@
 import lightning.pytorch as pl
 from omegaconf import OmegaConf
 
-from nemo.collections.tts.models import AudioCodecNewModel
+from nemo.collections.tts.models.audio_codec_new import AudioCodecNewModel
 from nemo.core.config import hydra_runner
 from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
