0bf8cf616a0dc583dd825ee4491ad3278e207488
Tristan Dobos <tristandobos37@gmail.com>
2026-03-21 11:36:06 +0100

    foc

--- nemo/collections/tts/modules/focalnet.py
+++ nemo/collections/tts/modules/focalnet.py
@@ -1209,7 +1209,7 @@ class FocalEncoder(nn.Module):
 
         encoded_len = audio_len
 
-        output = output.transpose(1, 2)
+        # output = output.transpose(1, 2)
 
         if self.debug:
             print("AFTER TRANSPOSE:")
