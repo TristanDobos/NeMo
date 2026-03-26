9667cf32c1931ef4ed9a5b711426f45a03d29a29
Tristan Dobos <tristandobos37@gmail.com>
2026-03-26 11:04:14 +0100

    fix

--- nemo/collections/tts/models/audio_codec.py
+++ nemo/collections/tts/models/audio_codec.py
@@ -567,7 +567,7 @@ class AudioCodecModel(ModelPT):
         # [B, D, T_encoded]
         encoded, encoded_len = self.audio_encoder(audio=audio, audio_len=audio_len)
 
-        encoded = self.compressor(encoded)
+
 
         if self.encoder_noise is not None:
             encoded = self.encoder_noise(encoded)
@@ -582,6 +582,10 @@ class AudioCodecModel(ModelPT):
                 commit_loss = 0.0
         else:
             commit_loss = 0.0
+
+        print(f"encoded before compressor: ", encoded.shape, encoded_len)
+        encoded = self.compressor(encoded)
+        print(f"encoded after compressor: ", encoded.shape, encoded_len)
 
         # [B, T]
         audio_gen, _ = self.audio_decoder(inputs=encoded, input_len=encoded_len)
