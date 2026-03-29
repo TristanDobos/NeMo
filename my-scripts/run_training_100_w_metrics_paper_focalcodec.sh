85060b77a8126ab14453ea54a8f4459b26af7024
Tristan Dobos <tristandobos37@gmail.com>
2026-03-29 17:27:58 +0200

    decrease gpus

--- my-scripts/run_training_100_w_metrics_paper_focalcodec.sh
+++ my-scripts/run_training_100_w_metrics_paper_focalcodec.sh
@@ -19,7 +19,7 @@ init_conda() {
  unset __conda_setup
 }
 
-. /mnt/matylda6/xdobos00/nemo_final/bin/activate
+. /mnt/matylda6/xdobos00/nemo_final/bin/activae
 
 export NEMO_DISABLE_ONE_LOGGER=1
 export WANDB_MODE=offline
@@ -72,7 +72,7 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # Run the Python command
 echo "Running: python /mnt/matylda6/xdobos00/NeMo/examples/tts/audio_codec.py --config-path $CONFIG_PATH --config-name $CONFIG_NAME"
-export CUDA_VISIBLE_DEVICES=$(~/scripts/free-gpus.sh 6)
+export CUDA_VISIBLE_DEVICES=$(~/scripts/free-gpus.sh 4)
 python /mnt/matylda6/xdobos00/NeMo/examples/tts/audio_codec.py --config-path "$CONFIG_PATH" --config-name "$CONFIG_NAME" 
 
 
